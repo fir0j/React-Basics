3fbdee5f92ce1171168383579d0a2dafd92ae1ec
fir0j <firoj.is.availble@gmail.com>
2019-07-25 01:33:17 +0530

    Updates

---
+++ precache-manifest.8483c7133710678ab7b02dab3194aec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d409e04743fdf7ec7a676d2844cdb6",
+    "url": "/fir0j/default-react-app/index.html"
+  },
+  {
+    "revision": "177fe319be3f46891191",
+    "url": "/fir0j/default-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/fir0j/default-react-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "177fe319be3f46891191",
+    "url": "/fir0j/default-react-app/static/js/main.d028f464.chunk.js"
+  },
+  {
+    "revision": "3f411c19b588568354b6",
+    "url": "/fir0j/default-react-app/static/js/runtime~main.28b3c428.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fir0j/default-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
